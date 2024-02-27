abfa64d57c92ff445d0da562834042818247308e
Mohithacker1 <125952416+Mohithacker1@users.noreply.github.com>
2024-02-27 21:06:56 +0530

    Add files via upload

---
+++ power.java
@@ -0,0 +1,10 @@
+public class power {
+    public static void main(String[] args) {
+        int a=1;
+        for(int i=1;i<=10;i++)
+        {
+            a=a<<1;
+            System.out.println(a);
+        }
+    }
+}
