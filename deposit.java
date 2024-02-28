d4feda046172ab1f5eea9d01d0d870524b9b2db5
Mohithacker1 <125952416+Mohithacker1@users.noreply.github.com>
2024-02-28 08:49:54 +0530

    Update

---
+++ deposit.java
@@ -0,0 +1,16 @@
+Public class deposite
+{
+Public static void main(String args[])
+{
+double deposite 1=5000, interest=0,total=0;
+
+Interest = deposited * 65/100;
+
+total = interest * 60;
+
+System.out.println("deposite = "+deposite 1); System.out.println("interest = "+ interest);
+
+System.out.println("total amount="+total);
+
+}
+}
